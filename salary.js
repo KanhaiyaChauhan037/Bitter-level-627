d679755b09f14b83afa84ccaa41716cf3efdf394
Navneet <singhnavneet.1711@gmail.com>
2022-08-26 00:43:17 +0530

    added day3

---
+++ salary.js
@@ -0,0 +1,6 @@
+document.getElementById("click1").addEventListener("click",function(){
+    document.querySelector(".bg-model").style.display='flex'
+});
+document.querySelector('.close').addEventListener("click",function(){
+    document.querySelector(".bg-model").style.display='none'
+});
