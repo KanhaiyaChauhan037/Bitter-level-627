41151f2b56fcf4ac441c0685e077988eab4ec264
kanhaiya chauhan <kanhaiyachauhan037@gmail.com>
2022-08-27 00:05:52 +0530

    added

---
+++ components/footer.js
@@ -0,0 +1,75 @@
+
+function footer (){
+
+return `
+    
+<div id="footer">
+    <div>
+        <h1>We're here to help</h1>
+        <p>Visit our Help Center for answers to common questions or contact us directly.</p>
+
+
+        <button>Help center</button>
+        <button>Contact support</button>
+
+    </div>
+
+    <div id="right">
+        <div>
+            <h3>Indeed</h3>
+           <p>About Indeed</p>
+           <p>Press</p>
+           <p>Security</p>
+           <p>Cookies</p>
+           <p>Terms</p>
+           <p>Privacy center</p>
+           <p>Privacy policy</p>
+           <p>Accessibility at Indeed</p>
+           <p>Work at Indeed</p>
+           <p>Countries
+        </p>
+           <p>Sitemap</p>
+          
+        </div>
+        <div>
+            <h3>Employers</h3>
+           <p>Post a job</p>
+           <p>Products</p>
+           <p>Pricing</p>
+           <p>Indeed Insights</p>
+           <p>FAQ</p>
+        </div>
+        <div>
+            <h3>Resources</h3>
+            <p>Hiring resources</p>
+            <p>How to write job descriptions</p>
+            <p>Guide to hiring with Indeed</p>
+            <p>Indeed Events</p>
+        </div>
+
+    </div>
+</div>
+<div id="foot">
+
+<p>© 2022 Indeed</p>
+<div>
+<img src="https://d34k7i5akwhqbd.cloudfront.net/allspark/static/images/icon-footer-ios-c1084f.svg" alt="">
+
+<img src="https://d34k7i5akwhqbd.cloudfront.net/allspark/static/images/icon-footer-android-7a4367.svg" alt="">
+
+<img src="https://d34k7i5akwhqbd.cloudfront.net/allspark/static/images/icon-footer-facebook-fa796a.svg" alt="">
+
+<img src="https://d34k7i5akwhqbd.cloudfront.net/allspark/static/images/icon-footer-twitter-cc3f5c.svg" alt="">
+
+<img src="https://d34k7i5akwhqbd.cloudfront.net/allspark/static/images/icon-footer-linkedin-ece97a.svg" alt="">
+
+<img src="https://d34k7i5akwhqbd.cloudfront.net/allspark/static/images/icon-footer-youtube-b0cacb.svg" alt="">
+
+<img src="https://d34k7i5akwhqbd.cloudfront.net/allspark/static/images/icon-footer-instagram-2827d1.svg" alt="">
+</div>
+
+</div>`
+
+}
+
+export default footer;
